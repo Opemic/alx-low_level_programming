f39cf9dec1820c4769190959ab5a74303a5080a0
opemic <oppyscholarship@gmail.com>
2022-12-13 14:33:31 -0800

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 
 /**
  * main - printer_putchar
- * 
+ *
  * Return: Always 0 (success)
  */
 
@@ -13,8 +13,8 @@ int main(void)
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
-        _putchar('t');
-        _putchar('c');
+	_putchar('t');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
