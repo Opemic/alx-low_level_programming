656744dcd683e282b414fda6c52fe58177a8547b
opemic <oppyscholarship@gmail.com>
2022-12-14 05:27:42 -0800

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -15,12 +15,11 @@ void print_alphabet_x10(void)
 	{
 		ch = 'a';
 		while (ch <= 'z')
+		{
 			_putchar(ch);
-		ch++;
+			ch++;
+		}
+		_putchar('\n');
+		i++;
 	}
-	_putchar('\n');
-	i++;
-
 }
-
-
