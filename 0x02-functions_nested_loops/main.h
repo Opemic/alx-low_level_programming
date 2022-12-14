3663f449c9ff08e84927ef2443ccb3f709717ef5
opemic <oppyscholarship@gmail.com>
2022-12-13 20:52:45 -0800

    0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 #ifndef main.h
 #define main.h
 
-int putchar(char);
+int _putchar(char);
 void print_alphabet(void);
-void print_alphabetx10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha (int c);
 int print_sign(int n);
