c01a9f88f5abd5d86cd9c1990df4cb5992671116
opemic <oppyscholarship@gmail.com>
2023-01-02 16:35:47 -0800

    8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,10 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *
  * print_diagsums - Entry point
  * @a: input
- * @size: input
+ * @size: input@
  * Return: Always 0 (Success)
  */
 void print_diagsums(int *a, int size)
