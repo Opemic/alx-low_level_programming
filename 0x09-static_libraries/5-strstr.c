6bfdbf675ce3af9d29bd748a3642526eef95d011
opemic <oppyscholarship@gmail.com>
2023-01-12 03:15:00 -0800

    0

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -11,11 +11,11 @@ char *_strstr(char *haystack, char *needle)
 	{
 		char *l = haystack;
 		char *p = needle;
-		
+
 		while (*l == *p && *p != '\0')
 		{
 			l++;
-			p++:
+			p++;
 		}
 		if (*p == '\0')
 			return (haystack);
